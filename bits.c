4f1e2e2a481df11425016555c08d46b54aabc34b
Daniel Esteban Maida <daniel.maida@wsu.edu>
2017-04-08 14:10:43 -0700

    Revert "Added a few comments. Changed a few things bits.c for readability"
    This reverts commit 51d3ca0a6b467a7f019b262b854bf60f6ec878cc

--- bits.c
+++ bits.c
@@ -1,10 +1,13 @@
 #include "bits.h"
 
+static int numOverflow = 0;
+static unsigned int overflow = 0;
+
 Bits* newBits(FILE* fd) {
   Bits* result = (Bits*) malloc(sizeof(Bits));
   result->fd = fd;
   result->extraBits = 0;
-  result->bitCount = 0; /* Number of bits extra bits*/
+  result->bitCount = 0;
   return result;
 }
 
@@ -13,59 +16,44 @@ void deleteBits(Bits* b) {
 }
 
 void writeBits(Bits* b, int nBits, int code){
-  /*
-  nBits is the number of bits to write.
-  code is the code that is going be written.
-  */
-  unsigned int c = 0;
-  int unsigned extraBits = b->extraBits;
-  int bitCount = b->bitCount;
+  unsigned int c;
 
-  bitCount += nBits;
+  numOverflow += nBits;
   code &= (1 << nBits) - 1;
 
-  extraBits = (extraBits << nBits) | code;
+  overflow = (overflow << nBits) | code;
 
-  while (bitCount >= CHAR_BIT) {
-  	bitCount -= CHAR_BIT;
-  	c = extraBits >> bitCount;
+  while (numOverflow >= CHAR_BIT) {
+  	numOverflow -= CHAR_BIT;
+  	c = overflow >> numOverflow;
   	fputc(c, b->fd);
-  	extraBits = extraBits ^ (c << bitCount); /* Bitwise xor*/
+  	overflow = overflow ^ (c << numOverflow); //bitwise xor
   }
 }
 
 bool readBits(Bits* b, int nBits, unsigned int *bits) {
-    /*
-    nBits is the number of bits to read.
-    Which should be between 8 and 24 bits.
-    *bits where the bits read are going to be saved.
-    */
-    int c = 0;
-    int unsigned extraBits = b->extraBits;
-    int bitCount = b->bitCount;
+    int c;
+    static int numOverflow = 0;
+    static int unsigned overflow = 0;
 
-    while (bitCount < nBits) {
+    while (numOverflow < nBits) {
       if ((c = fgetc(b->fd)) == EOF){
         return false;
       }
-      bitCount += CHAR_BIT;
-      extraBits = (extraBits << CHAR_BIT) | c;
+      numOverflow += CHAR_BIT;
+      overflow = (overflow << CHAR_BIT) | c;
     }
-    bitCount -= nBits;
-    c = extraBits >> bitCount;
-    extraBits = extraBits ^ (c << bitCount);
+    numOverflow -= nBits;
+    c = overflow >> numOverflow;
+    overflow = overflow ^ (c << numOverflow);
     *bits = c;
-    b->extraBits = extraBits;
-    b->bitCount = bitCount;
     return true;
 }
 
-void flushBits(Bits* b) {
+void sendRemainingBits(Bits* b) {
   char c;
-  int unsigned extraBits = b->extraBits;
-  int bitCount = b->bitCount;
-  if (bitCount != 0){
-    c = extraBits << (CHAR_BIT - bitCount);
+  if (numOverflow != 0){
+    c = overflow << (CHAR_BIT - numOverflow);
   	fputc(c, b->fd);
   }
 }
